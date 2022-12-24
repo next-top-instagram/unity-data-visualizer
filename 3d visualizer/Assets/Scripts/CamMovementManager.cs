69f17c742f73a6ca27611b6e93b55e647fef75e2
Kim HyunWoo <stories2@naver.com>
2022-12-25 03:01:44 +0900

    remove hardcoded

--- 3d visualizer/Assets/Scripts/CamMovementManager.cs
+++ 3d visualizer/Assets/Scripts/CamMovementManager.cs
@@ -7,6 +7,11 @@ public class CamMovementManager : MonoBehaviour
     // https://docs.unity3d.com/ScriptReference/RangeAttribute.html
     [Range(0.5f, 3)]
     public float movementSpeed = 1f;
+
+    readonly float MIN_X_RANGE = -2.3f;
+    readonly float MAX_X_RANGE = 1.25f;
+    readonly float MIN_Y_RANGE = -0.6f;
+    readonly float MAX_Y_RANGE = 3.3f;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,24 +28,24 @@ public class CamMovementManager : MonoBehaviour
             // https://docs.unity3d.com/kr/530/ScriptReference/Mathf.Clamp.html
             gameObject.transform.position = new Vector3(
                 gameObject.transform.position.x, 
-                Mathf.Clamp(gameObject.transform.position.y + movementSpeed * Time.deltaTime, -0.6f, 3.3f),
+                Mathf.Clamp(gameObject.transform.position.y + movementSpeed * Time.deltaTime, MIN_Y_RANGE, MAX_Y_RANGE),
                 gameObject.transform.position.z);
         }
         if (Input.GetKey(KeyCode.A)) {
             gameObject.transform.position = new Vector3(
-                Mathf.Clamp(gameObject.transform.position.x - movementSpeed * Time.deltaTime, -2.3f, 1.25f),
+                Mathf.Clamp(gameObject.transform.position.x - movementSpeed * Time.deltaTime, MIN_X_RANGE, MAX_X_RANGE),
                 gameObject.transform.position.y, 
                 gameObject.transform.position.z);
         }
         if (Input.GetKey(KeyCode.S)) {
             gameObject.transform.position = new Vector3(
                 gameObject.transform.position.x, 
-                Mathf.Clamp(gameObject.transform.position.y - movementSpeed * Time.deltaTime, -0.6f, 3.3f),
+                Mathf.Clamp(gameObject.transform.position.y - movementSpeed * Time.deltaTime, MIN_Y_RANGE, MAX_Y_RANGE),
                 gameObject.transform.position.z);
         }
         if (Input.GetKey(KeyCode.D)) {
             gameObject.transform.position = new Vector3(
-                Mathf.Clamp(gameObject.transform.position.x + movementSpeed * Time.deltaTime, -2.3f, 1.25f), 
+                Mathf.Clamp(gameObject.transform.position.x + movementSpeed * Time.deltaTime, MIN_X_RANGE, MAX_X_RANGE), 
                 gameObject.transform.position.y, 
                 gameObject.transform.position.z);
         }
